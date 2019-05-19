3cf849bfd6135969827a901cce0ba8f672640a49
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-19 20:56:39 +0000

    Reformat packets input and predict output formats

--- kddcup/detector.py
+++ kddcup/detector.py
@@ -8,22 +8,22 @@ from preprocessor import Preprocessor
 
 
 class Detector(object):
-  def __init__(self, preprocessor, model):
-    self.preprocessor = preprocessor
-    self.model = model
-    
-  @classmethod
-  def from_path(cls, encoders_path, model_path):
-    prep = Preprocessor.load(encoders_path)
-    mdl = tf.keras.models.load_model(model_path)
-    return cls(prep, mdl)
-    
-  def predict(self, data):
-    dataX = self.preprocessor.transformX(data)
-    pred = self.model.predict(dataX)
-    pred = np.round(pred)
-    return self.preprocessor.inverse_transformY(pred).tolist()
-  
-  def predict_csv(self, csvfile):
-    csvdata = pd.read_csv(csvfile)
-    return self.predict(csvdata)
+    def __init__(self, preprocessor, model):
+        self.preprocessor = preprocessor
+        self.model = model
+
+    @classmethod
+    def from_path(cls, encoders_path, model_path):
+        prep = Preprocessor.load(encoders_path)
+        mdl = tf.keras.models.load_model(model_path)
+        return cls(prep, mdl)
+
+    def predict(self, data):
+        dataX = self.preprocessor.transformX(data)
+        pred = self.model.predict(dataX)
+        pred = np.round(pred)
+        return self.preprocessor.inverse_transformY(pred)[:, 0].tolist()
+
+    def predict_csv(self, csvfile):
+        csvdata = pd.read_csv(csvfile)
+        return self.predict(csvdata)
--- kddcup/kddcup.py
+++ kddcup/kddcup.py
@@ -13,19 +13,22 @@ example_malicious_packet = ["0", "icmp", "ecr_i", "SF", "1032", "0", "0", "0", "
 example_normal_packet = ["0","tcp","http","SF","184","124","0","0","0","0","0","1","0","0","0","0","0","0","0","0","0","0","1","1","0.00","0.00","0.00","0.00","1.00","0.00","0.00","10","10","1.00","0.00","0.10","0.00","0.00","0.00","0.00","0.00"]
 
 
-def fix_types(packet):
-    last_floats = [float(p) for p in packet[4:]]
-    clean_packet = [float(packet[0]), packet[1],
-                    packet[2], packet[3]] + last_floats
-    return clean_packet
+def fix_types(packets):
+    clean_packets = []
+    for packet in packets:
+        last_floats = [float(p) for p in packet[4:]]
+        clean_packet = [float(packet[0]), packet[1],
+                        packet[2], packet[3]] + last_floats
+        clean_packets.append(clean_packet)
+    return clean_packets
 
 
 class Query(graphene.ObjectType):
-    predict = graphene.String(packet=graphene.List(graphene.String))
+    predict = graphene.List(graphene.String, packets=graphene.List(graphene.List(graphene.String)))
 
-    def resolve_predict(self, info, packet):
+    def resolve_predict(self, info, packets):
         # Cast number strings in packet to their types (float here)
-        clean_packet = fix_types(packet)
+        clean_packets = fix_types(packets)
 
         # Load preprocessors and model for prediction
         encoders_dir = 'kddcup/encoders'
@@ -35,10 +38,10 @@ class Query(graphene.ObjectType):
         # Format packet to a dataframe format
         header = ['0', 'tcp', 'http', 'SF', '184', '124', '0.1', '0.2', '0.3', '0.4', '0.5', '1', '0.6', '0.7', '0.8', '0.9', '0.10', '0.11', '0.12', '0.13', '0.14', '0.15', '1.1',
                   '1.2', '0.00', '0.00.1', '0.00.2', '0.00.3', '1.00', '0.00.4', '0.00.5', '10', '10.1', '1.00.1', '0.00.6', '0.10.1', '0.00.7', '0.00.8', '0.00.9', '0.00.10', '0.00.11']
-        dfX = pd.DataFrame([clean_packet], columns=header)
+        dfX = pd.DataFrame(clean_packets, columns=header)
 
         # Predict
-        return d.predict(dfX)[0]
+        return d.predict(dfX)
 
 
 schema = graphene.Schema(query=Query)
