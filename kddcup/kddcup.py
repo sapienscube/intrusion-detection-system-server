1c2a099fd77a8d2538cd5dc563623dafb3dcaca2
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2019-05-19 17:27:12 +0000

    Imported main code to kddcup: successful

--- kddcup/kddcup.py
+++ kddcup/kddcup.py
@@ -3,7 +3,23 @@ from flask import Flask
 from flask_graphql import GraphQLView
 import pandas as pd
 
+from detector import Detector
+
 app = Flask(__name__)
+
+encoders_dir = 'kddcup/encoders'
+model_path = 'kddcup/models/kddcup-model-loss-0.0033-acc-99.94.h5'
+
+d = Detector.from_path(encoders_dir, model_path)
+
+header = ['0', 'tcp', 'http', 'SF', '184', '124', '0.1', '0.2', '0.3', '0.4', '0.5', '1', '0.6', '0.7', '0.8', '0.9', '0.10', '0.11', '0.12', '0.13', '0.14', '0.15', '1.1',
+          '1.2', '0.00', '0.00.1', '0.00.2', '0.00.3', '1.00', '0.00.4', '0.00.5', '10', '10.1', '1.00.1', '0.00.6', '0.10.1', '0.00.7', '0.00.8', '0.00.9', '0.00.10', '0.00.11']
+
+packet = [0, 'icmp', 'ecr_i', 'SF', 1032, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          511, 511, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 255, 255, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+dfX = pd.DataFrame([packet], columns=header)
+print(d.predict(dfX))
 
 
 def fix_types(packet):
